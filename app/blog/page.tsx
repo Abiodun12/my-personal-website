a244e2f99cdb57e0f9c35f9803c98b0d6d5a7520
Abiodun12 <Soneyebiodun@gmail.com>
2025-01-20 17:40:03 -0600

    fix: revert to working blog layout
    - Restore previous working layout structure
    - Match styling with other pages
    - Fix terminal container nesting
    - Maintain consistent page structure

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,37 +1,28 @@
 'use client'
 
 import React from 'react'
-import { Terminal } from '@/components/Terminal'
-import { TerminalLink } from '@/components/TerminalLink'
-import { Cursor } from '@/components/Cursor'
-import { blogPosts } from '@/config/blog'
+import { Terminal } from '../../components/Terminal'
+import { TerminalLink } from '../../components/TerminalLink'
+import { blogPosts } from '../../config/blog'
 
 export default function BlogPage() {
   return (
-    <div className="terminal-content">
+    <main style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+      <Terminal>$ ls blog/</Terminal>
       <Terminal>
-        $ ls blog/
-        <Cursor />
-      </Terminal>
-
-      <Terminal>
-        {blogPosts.map((post, index) => (
+        Available posts:
+        {blogPosts.map((post) => (
           <React.Fragment key={post.slug}>
-            <div className="blog-entry">
-              <TerminalLink href={post.url}>{post.title}</TerminalLink>
-              <br />
-              <span style={{ color: '#666' }}>{post.date}</span>
-              <br />
-              {post.description}
-            </div>
-            {index < blogPosts.length - 1 && <br /><br />}
+            <br />
+            - <TerminalLink href={post.url}>{post.title}</TerminalLink> ({post.date})
+            <br />
+            &nbsp;&nbsp;{post.description}
           </React.Fragment>
         ))}
       </Terminal>
-
       <Terminal>
         $ <TerminalLink href="/">cd ..</TerminalLink>
       </Terminal>
-    </div>
+    </main>
   )
 } 
