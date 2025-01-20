70e428dcdad8c0381b900c37ec499e4ca21de317
Abiodun12 <Soneyebiodun@gmail.com>
2025-01-20 17:32:58 -0600

    fix: resolve Vercel build errors and improve terminal layout
    - Use Terminal component as wrapper
    - Add terminal-section styling
    - Improve content organization
    - Maintain terminal theme and functionality

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,28 +8,32 @@ import { blogPosts } from '../../config/blog'
 
 export default function BlogPage() {
   return (
-    <div className="main-container">
-      <Terminal>
-        $ ls blog/
-        <Cursor />
-      </Terminal>
-      <Terminal>
-        {blogPosts.map((post, index) => (
-          <React.Fragment key={post.slug}>
-            <div className="blog-entry">
-              <TerminalLink href={post.url}>{post.title}</TerminalLink>
-              <br />
-              <span style={{ color: '#666' }}>{post.date}</span>
-              <br />
-              {post.description}
-            </div>
-            {index < blogPosts.length - 1 && <br /><br />}
-          </React.Fragment>
-        ))}
-      </Terminal>
-      <Terminal>
-        $ <TerminalLink href="/">cd ..</TerminalLink>
-      </Terminal>
-    </div>
+    <Terminal>
+      <div className="terminal-content">
+        <div className="terminal-section">
+          $ ls blog/
+          <Cursor />
+        </div>
+
+        <div className="terminal-section">
+          {blogPosts.map((post, index) => (
+            <React.Fragment key={post.slug}>
+              <div className="blog-entry">
+                <TerminalLink href={post.url}>{post.title}</TerminalLink>
+                <br />
+                <span style={{ color: '#666' }}>{post.date}</span>
+                <br />
+                {post.description}
+              </div>
+              {index < blogPosts.length - 1 && <br /><br />}
+            </React.Fragment>
+          ))}
+        </div>
+
+        <div className="terminal-section">
+          $ <TerminalLink href="/">cd ..</TerminalLink>
+        </div>
+      </div>
+    </Terminal>
   )
 } 
